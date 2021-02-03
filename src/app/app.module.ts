bfc50c8f16b979a2e03fa52dd382521eda4b0fee
Lena Vu <lenavu@Lenas-MacBook-Pro.local>
2021-02-03 09:41:11 +0200

    deleted unnecessary files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { MaterialModule } from './material/material.module';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimerComponent } from './timer/timer.component';
 import { MatButtonModule } from '@angular/material/button';
@@ -17,9 +15,8 @@ import { FormatPipe } from './pipes/time-format/time-format.pipe'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MaterialModule,
-    MatButtonModule
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
